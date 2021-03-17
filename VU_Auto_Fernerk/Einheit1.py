a3f2351dd16fa0605770d1588efd5dc613f25bab
Tabernig <ronald.tabernig@student.uibk.ac.at>
2021-03-17 08:12:10 +0100

    add skripts

---
+++ VU_Auto_Fernerk/Einheit1.py
@@ -0,0 +1,44 @@
+import pclpy
+import os
+import sys
+import numpy as np
+
+os.chdir("D:/_Programmieren/repos/skript/VU_Automatisierung") ####UPDATE
+
+fobj = open("./Daten/testcoordinates.txt","r")
+myfile = []
+for line in fobj:
+    line= line.strip()
+    getrennte_Line = line.split(" ")
+    print(getrennte_Line)
+    row = []
+    for element in getrennte_Line:
+        value = float(element)
+        row.append(value)
+        print(value)
+    myfile.append(row)
+
+myfileArray = np.array(myfile)
+print(myfileArray)
+
+
+#print(type(fobj))
+fobj.close()
+
+fobj_Out = open("./Daten/ausgabe.txt","w")
+for myline in myfileArray:
+    i = 0
+    for element in myline:
+        fobj_Out.write(str(element))
+        if i == 0:
+            fobj_Out.write(";")
+            i+=1
+    fobj_Out.write("\n")
+
+fobj_Out.close()
+
+np.savetxt("./Daten/ausgabeNp.txt",myfileArray)
+
+
+print("Done.")
+
