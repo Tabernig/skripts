522341bb7eff800bc5cf06c957ee6c15ebc67452
Tabernig <ronald.tabernig@student.uibk.ac.at>
2021-03-17 09:26:44 +0100

    intensity done

--- VU_Fernerk/Mitschriften/intensity.py
+++ VU_Fernerk/Mitschriften/intensity.py
@@ -2,6 +2,8 @@
 
 #MODULE
 import argparse
+import re
+import numpy as np
 
 #Parser
 #Definition
@@ -27,7 +29,37 @@ print(args.threshold)
 
 #Hauptprogramm
 
+#Datei Einlesen
+fobj = open(args.infile,"r")        #Eingabefile im Lesemodus
+fobj_Out = open(args.outfile,"w")   #Ausgabefile im Schreibmodus öffnen
+
+
+
+#Spalten aufteilen
+myfile = []
+count = 0 #fehlerhafte punkte
+for line in fobj:
+    line= line.strip() #entfernen des Zeilenumbruchs
+    getrennte_Line = re.split(args.delimiter,line)
+    colidx = args.colno - 1 #Umwaltung der Spaltennummer in Index
+    intensity = getrennte_Line.pop(colidx)
+    if float(intensity) <= args.threshold:  #Intensitätswerte > Schwellenwert entfernen
+        getrennte_Line.insert(colidx,str(intensity)) #passende Intensitätswerte werden in Line an ursprünglicher Position wieder eingefügt
+        outstring = ",".join(getrennte_Line) #Umwandlung der Liste in einen String
+        outstring = outstring +"\n"
+        fobj_Out.write(outstring) #Datei schreiben
+    else:
+        count += 1
+
+print(count)
+
+
+fobj.close()
+fobj_Out.close()
 
 print("Done")
 
+#Standard Input
 #D:\_Programmieren\VU_Automatisierung_Daten\Daten\pointcloud1_small.txt
+#D:\_Programmieren\VU_Automatisierung_Daten\Daten\pointcloud1_small_corrected.txt
+#python intensity.py -infile D:\_Programmieren\VU_Automatisierung_Daten\Daten\pointcloud1_small.txt -outfile D:\_Programmieren\VU_Automatisierung_Daten\Daten\pointcloud1_small_corrected.txt
